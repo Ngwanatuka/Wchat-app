a4bcc152ab3147be4ded93a3ac29485e5cc07ff4
Ngwanatuka <ngwanatuka@gmail.com>
2023-12-13 23:09:16 +0200

    Update devServer static path in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         ],
     },
     devServer: {
-        static: path.resolve(__dirname, 'dist'),
+        static: path.resolve(__dirname, 'public'),
         port: 9000,
         open: true
     }
