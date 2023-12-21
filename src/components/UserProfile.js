e8b77948c2bb006e137d60e2a66a669a1ed2b280
Ngwanatuka <ngwanatuka@gmail.com>
2023-12-21 18:25:50 +0200

    Refactor UserProfile component styling

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,22 +1,22 @@
-import React, { useState, useRef } from 'react';
-import { StyleSheet, css } from 'aphrodite';
+import React, { useState, useRef } from "react";
+import { StyleSheet, css } from "aphrodite";
 
 const UserProfile = ({ userEmail }) => {
-  const [knownAs, setKnownAs] = useState('');
-  const [department, setDepartment] = useState('');
-  const [profession, setProfession] = useState('');
-  const [aboutMe, setAboutMe] = useState('');
+  const [knownAs, setKnownAs] = useState("");
+  const [department, setDepartment] = useState("");
+  const [profession, setProfession] = useState("");
+  const [aboutMe, setAboutMe] = useState("");
   const [profilePicture, setProfilePicture] = useState(null);
   const fileInputRef = useRef(null);
 
   const handleNextButtonClick = () => {
     // Handle the logic for navigating to the dashboard or processing the form data
     // For now, let's log the data to the console
-    console.log('Known As:', knownAs);
-    console.log('Department:', department);
-    console.log('Profession:', profession);
-    console.log('About Me:', aboutMe);
-    console.log('Profile Picture:', profilePicture);
+    console.log("Known As:", knownAs);
+    console.log("Department:", department);
+    console.log("Profession:", profession);
+    console.log("About Me:", aboutMe);
+    console.log("Profile Picture:", profilePicture);
   };
 
   const handleProfilePictureChange = (e) => {
@@ -39,16 +39,21 @@ const UserProfile = ({ userEmail }) => {
         <div className={css(styles.userInfo)}>
           <div className={css(styles.profileInfo)}>
             {/* Profile picture with plus sign icon */}
-            <div className={css(styles.profilePhoto)} onClick={handlePlusSignClick}>
+            <div
+              className={css(styles.profilePhoto)}
+              onClick={handlePlusSignClick}
+            >
               {profilePicture ? (
-                <>
-                  <img src={URL.createObjectURL(profilePicture)} alt="Profile" />
-                  <div className={css(styles.plusSign)}>+</div>
-                </>
+                <img src={URL.createObjectURL(profilePicture)} alt="Profile" />
               ) : (
-                <div className={css(styles.plusSignContainer)}>
-                  <div className={css(styles.plusSign)}>+</div>
-                </div>
+                <>
+                  <div className={css(styles.defaultUserIcon)}>
+                    <i className="fa fa-user"></i>
+                  </div>
+                  <div className={css(styles.plusSignContainer)}>
+                    <div className={css(styles.plusSign)}>+</div>
+                  </div>
+                </>
               )}
             </div>
             {/* User email */}
@@ -91,7 +96,10 @@ const UserProfile = ({ userEmail }) => {
             />
           </div>
           {/* Next button */}
-          <button className={css(styles.nextButton)} onClick={handleNextButtonClick}>
+          <button
+            className={css(styles.nextButton)}
+            onClick={handleNextButtonClick}
+          >
             Next
           </button>
         </div>
@@ -102,7 +110,7 @@ const UserProfile = ({ userEmail }) => {
         id="profilePictureInput"
         accept="image/*"
         ref={fileInputRef}
-        style={{ display: 'none' }}
+        style={{ display: "none" }}
         onChange={handleProfilePictureChange}
       />
     </div>
@@ -111,105 +119,119 @@ const UserProfile = ({ userEmail }) => {
 
 const styles = StyleSheet.create({
   userProfile: {
-    textAlign: 'center',
+    textAlign: "center",
+    display: "flex",
+    width: "100%",
+    backgroundColor: "#0986CC",
+    fontFamily: "Poppins, sans-serif",
+  },
+
+  defaultUserIcon: {
+    fontSize: '2rem',
+    color: '#fff',
     display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     width: '100%',
-    backgroundColor: '#0986CC',
-    fontFamily: 'Poppins, sans-serif',
+    height: '100%',
   },
 
   leftContainer: {
-    width: '35%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "35%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   welcomeMessage: {
-    color: 'white',
-    fontSize: '1.5rem',
-    fontWeight: 'bold',
-    lineHeight: '1.5',
+    color: "white",
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    lineHeight: "1.5",
   },
 
   profileContainer: {
-    width: '65%',
-    height: '100%',
-    backgroundColor: 'white',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    boxShadow: '0 0 1rem 0 rgba(0, 0, 0, .2)',
+    width: "65%",
+    height: "100%",
+    backgroundColor: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   userInfo: {
-    textAlign: 'left',
+    textAlign: "left",
   },
 
   profileInfo: {
-    display: 'flex',
-    alignItems: 'center',
-    marginBottom: '10px',
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "10px",
   },
 
   profilePhoto: {
-    position: 'relative',
-    width: '50px',
-    height: '50px',
-    borderRadius: '50%',
-    overflow: 'hidden',
-    marginRight: '10px',
-    cursor: 'pointer', // Add cursor pointer for click interaction
+    position: "relative",
+    width: "50px",
+    height: "50px",
+    borderRadius: "50%",
+    overflow: "hidden",
+    marginRight: "10px",
+    cursor: "pointer", // Add cursor pointer for click interaction
+    background: "#0986CC",
   },
 
   plusSignContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Add background for better visibility
+    position: "absolute",
+    bottom: "0",
+    right: "0",
+    background: "#0986CC",
+    color: "white",
+    borderRadius: "50%",
+    width: "20px",
+    height: "20px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   plusSign: {
-    color: 'white',
-    fontSize: '1.5rem',
+    fontSize: "1.5rem",
   },
 
   userEmail: {
-    color: '#0986CC',
-    fontSize: '1rem',
-    margin: '0',
+    color: "#0986CC",
+    fontSize: "1rem",
+    margin: "0",
   },
 
   formContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    marginBottom: '10px',
+    display: "flex",
+    flexDirection: "column",
+    marginBottom: "10px",
   },
 
   formLabel: {
-    marginBottom: '5px',
-    fontSize: '0.9rem',
+    marginBottom: "5px",
+    fontSize: "0.9rem",
+    color: "#333",
   },
 
   formInput: {
-    width: '100%',
-    padding: '8px',
-    borderRadius: '4px',
-    border: '1px solid #ccc',
+    width: "100%",
+    padding: "8px",
+    borderRadius: "4px",
+    border: "1px solid #ccc",
+    fontFamily: "Poppins, sans-serif",
   },
 
   nextButton: {
-    backgroundColor: '#0986CC',
-    color: 'white',
-    padding: '10px',
-    borderRadius: '4px',
-    border: 'none',
-    cursor: 'pointer',
+    backgroundColor: "#0986CC",
+    color: "white",
+    padding: "10px",
+    borderRadius: "4px",
+    border: "none",
+    cursor: "pointer",
+    fontFamily: "Poppins, sans-serif",
   },
 });
 
