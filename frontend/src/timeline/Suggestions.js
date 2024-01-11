5fca3e89f6ba4926dff0e8f2c141ad295f60357f
Ngwanatuka <ngwanatuka@gmail.com>
2024-01-11 12:49:17 +0200

    Update import statement in Suggestions.js and fix hover effect in Post.js

--- frontend/src/timeline/Suggestions.js
+++ frontend/src/timeline/Suggestions.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { StyleSheet, css } from "aphrodite";
+import { StyleSheet, css } from "aphrodite-jss";
 import { Avatar } from "@mui/material";
 
-function Suggestions() {
+function Suggestions(user) {
   return (
     <div className={css(styles.suggestions)}>
       <div className={css(styles.suggestions__title)}>Suggestions For You</div>
       <div className={css(styles.seggestions__userNames)}>
-        
         <div className={css(styles.suggestions__username)}>
           <div className={css(styles.username__left)}>
             <span className={css(styles.avatar)}>
@@ -15,24 +14,7 @@ function Suggestions() {
             </span>
             <div className={css(styles.username__infor)}>
               <span className={css(styles.username__name)}>ngwanatuka</span>
-              <span className={css(styles.relation)}>
-                New to Wchat
-              </span>
-            </div>
-          </div>
-          <div className={css(styles.follow__button)}>Follow</div>
-        </div>
-      
-        <div className={css(styles.suggestions__username)}>
-          <div className={css(styles.username__left)}>
-            <span className={css(styles.avatar)}>
-              <Avatar>R</Avatar>
-            </span>
-            <div className={css(styles.username__infor)}>
-              <span className={css(styles.username__name)}>ngwanatuka</span>
-              <span className={css(styles.relation)}>
-                New to Wchat
-              </span>
+              <span className={css(styles.relation)}>New to Wchat</span>
             </div>
           </div>
           <div className={css(styles.follow__button)}>Follow</div>
@@ -45,19 +27,81 @@ function Suggestions() {
             </span>
             <div className={css(styles.username__infor)}>
               <span className={css(styles.username__name)}>ngwanatuka</span>
-              <span className={css(styles.relation)}>
-                New to Wchat
-              </span>
+              <span className={css(styles.relation)}>New to Wchat</span>
             </div>
           </div>
           <div className={css(styles.follow__button)}>Follow</div>
         </div>
 
+        <div className={css(styles.suggestions__username)}>
+          <div className={css(styles.username__left)}>
+            <span className={css(styles.avatar)}>
+              <Avatar>R</Avatar>
+            </span>
+            <div className={css(styles.username__infor)}>
+              <span className={css(styles.username__name)}>ngwanatuka</span>
+              <span className={css(styles.relation)}>New to Wchat</span>
+            </div>
+          </div>
+          <div className={css(styles.follow__button)}>Follow</div>
+        </div>
       </div>
     </div>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  suggestions: {
+    marginTop: "30px",
+    marginRight: "20px",
+  },
+
+  suggestions__title: {
+    color: "grey",
+    fontWeight: "bolder",
+  },
+
+  username__left: {
+    display: "flex",
+    alignItems: "center",
+  },
+
+  username__infor: {
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  relation: {
+    color: "grey",
+    fontSize: "12px",
+  },
+
+  suggestions__username: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: "15px",
+    
+  },
+
+  follow__button: {
+    color: "#0095f6",
+    background: "transparent",
+    border: "0",
+    "&:hover": {
+      cursor: "pointer",
+      color: "grey",
+    },
+  },
+
+  username__name: {
+    fontWeight: 600,
+    fontSize: "14px",
+  },
+  
+avatar: {
+  marginRight: "10px",
+},
+
+});
 
 export default Suggestions;
--- frontend/src/timeline/posts/Post.js
+++ frontend/src/timeline/posts/Post.js
@@ -84,7 +84,7 @@ const styles = StyleSheet.create({
 
     alignItems: "center",
     justifyContent: "space-between",
-    ":hover": {
+    "&:hover": {
       cursor: "pointer",
     },
   },
