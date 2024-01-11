c373f24fbbf71a68b3fd3f4364e735a8d607a9e8
Ngwanatuka <ngwanatuka@gmail.com>
2024-01-11 23:01:55 +0200

    Add Redux Toolkit and update authentication logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,15 @@ import './App.css'
 import Athentication from './Athentication/Athentication'
 
 function App() {
+  const user = useSelector(state => state.user.user)
+  console.log(user)
   return (
     <div className='app'>
-    <Athentication />
-        {/* <Homepage /> */}
+    {user ? (
+      <Homepage />
+    ) : (
+      <Athentication />
+    )}
     </div>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
-// index.js
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
