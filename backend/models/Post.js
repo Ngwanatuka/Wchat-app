7e317d68e5068f65cfb251d0d6d09ecaf9c6f457
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2024-01-17 23:55:29 +0100

    Added posts

---
+++ backend/models/Post.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Post = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference to the User model
+    required: true,
+  },
+  imageUrl: {
+    type: String, // image link on cloud
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const PostModel = mongoose.model('Post', Post);
+
+module.exports = PostModel;
