3ab8b3d8f07d8ca4aa5d78d71ce36e876baa2ffc
Ngwanatuka <ngwanatuka@gmail.com>
2024-01-17 02:52:26 +0200

    Add Comments component and update Post and Profile components

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./post.scss";
 // import FavoutOutlinedIcon from "@mui/icons-material";
 // import FavoriteBorderOutlinedIcon from "@mui/icons-material";
@@ -8,8 +8,11 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
+import Comments from "../comments/Comments";
 
 function Post({ post }) {
+  const [commentOpen, setCommentOpen] = useState(false);
+
   // temp
 
   const liked = false;
@@ -40,13 +43,14 @@ function Post({ post }) {
             {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
             12 likes
           </div>
-          <div className="items">
+          <div className="items" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon /> 12 comments
           </div>
           <div className="items">
-            <ShareOutlinedIcon /> 12 likes
+            <ShareOutlinedIcon /> Share
           </div>
         </div>
+        {commentOpen && <Comments />}
       </div>
     </div>
   );
