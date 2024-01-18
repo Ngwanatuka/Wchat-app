a2f52fbc32327f00f950f856b97920299b41e23b
Ngwanatuka <ngwanatuka@gmail.com>
2024-01-18 02:02:39 +0200

    Fix API route paths and add user creation functionality

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -2,21 +2,34 @@ import { db } from "../connect.js";
 import bcrypt from "bcryptjs";
 
 export const login = (req, res) => {
+  // Check if the user exists
 
-    // Check if the user exists
+  const q = "SELECT * FROM users WHERE username = ?";
 
-    const q = "SELECT * FROM users WHERE username = ?"
-
-    db.query(q, [req.body.username], (err, data)=> {
-        if(err) return res.status(500).json(err);
-        if(data.length) return res.status(409).json('User already exists')
-    })
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length) return res.status(409).json("User already exists");
 
     // Create a new user
-        // hash the password
-        const salt = bcrypt.genSaltSync(10);
-        const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    // hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
 
+    const q =
+      "INSERT INTO users (`username`, `email`, `password`, `name` VALUES (?)";
+
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("User created successfully");
+    });
+  });
 };
 
 export const register = (req, res) => {};
