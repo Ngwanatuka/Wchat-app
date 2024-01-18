6c16c8e551bab2b0dc07c10010630024e269103f
Ngwanatuka <ngwanatuka@gmail.com>
2024-01-18 04:57:40 +0200

    Fix import statement and add axios dependency

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import  Express  from "express";
+import Express from "express";
 const app = Express();
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
@@ -8,12 +8,18 @@ import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-
 // middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(Express.json());
-app.use(cors());
-app.use(cookieParser())
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
@@ -21,7 +27,6 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
 app.use("/api/auth", authRoutes);
 
-
 app.listen(8800, () => {
-    console.log("API working")
+  console.log("API working");
 });
