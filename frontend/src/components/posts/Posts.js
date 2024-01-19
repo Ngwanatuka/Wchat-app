ff08426e5a8d1b6462f17c6159d6b5d1d75891dc
Ngwanatuka <ngwanatuka@gmail.com>
2024-01-19 12:26:56 +0200

    Refactor Posts component to use react-query for fetching data

--- frontend/src/components/posts/Posts.js
+++ frontend/src/components/posts/Posts.js
@@ -5,14 +5,21 @@ import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 
 function Posts() {
+  const { isLoading, error, data } = useQuery(["posts"], () =>
+    makeRequest.get("/posts?userId=" + userId).then((res) => {
+      return res.data;
+    })
+  );
 
-
-
-  return <div className="posts">
-    {posts.map(post=> (
-       <Post post={post} key={post.id}/>
-    ))}
-  </div>;
+  return (
+    <div className="posts">
+      {error
+        ? "Something went wrong!"
+        : isLoading
+        ? "loading"
+        : data.map((post) => <Post post={post} key={post.id} />)}
+    </div>
+  );
 }
 
 export default Posts;
