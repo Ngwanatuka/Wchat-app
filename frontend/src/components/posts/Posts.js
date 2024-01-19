961199ce09efc48c9cdd4d3bbf5174a2f654e221
Ngwanatuka <ngwanatuka@gmail.com>
2024-01-19 17:39:04 +0200

    Remove unused code and update API endpoint

--- frontend/src/components/posts/Posts.js
+++ frontend/src/components/posts/Posts.js
@@ -6,7 +6,7 @@ import { makeRequest } from "../../axios";
 
 function Posts() {
   const { isLoading, error, data } = useQuery(["posts"], () =>
-    makeRequest.get("/posts?userId=" + userId).then((res) => {
+    makeRequest.get("/posts").then((res) => {
       return res.data;
     })
   );
