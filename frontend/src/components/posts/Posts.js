3c5d2b27f5f10779f93e749bbe8c66f1480bf562
Ngwanatuka <ngwanatuka@gmail.com>
2024-01-23 19:09:25 +0200

    Update API and frontend code

--- frontend/src/components/posts/Posts.js
+++ frontend/src/components/posts/Posts.js
@@ -1,19 +1,15 @@
 import React from "react";
-import "./posts.scss";
 import Post from "../post/Post";
+import "./posts.scss";
 import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 
-function Posts({ userId }) {
-  const { isLoading, error, data } = useQuery({
-    queryKey: ["posts", { userId }],
-    queryFn: () =>
-      makeRequest.get(`/posts?userId=${userId}`).then((res) => {
-        return res.data;
-      }),
-  });
-  
-  console.log(data);
+const Posts = ({userId}) => {
+  const { isLoading, error, data } = useQuery(["posts"], () =>
+    makeRequest.get("/posts?userId="+userId).then((res) => {
+      return res.data;
+    })
+  );
 
   return (
     <div className="posts">
@@ -24,6 +20,6 @@ function Posts({ userId }) {
         : data.map((post) => <Post post={post} key={post.id} />)}
     </div>
   );
-}
+};
 
 export default Posts;
